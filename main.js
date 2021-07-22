4842de6e18c98a8d40e1d4b88cfcd0966f04e12d
Luciano <lucco6823@gmail.com>
2021-07-22 12:17:08 +0200

    Header + input

---
+++ main.js
@@ -0,0 +1,11 @@
+var btnGenera = document.getElementById("genera");
+console.log(btnGenera);
+var nome, km, fascia_eta;
+
+btnGenera.addEventListener("click",
+    function{
+        nome = document.getElementById("nome").value;
+        km = document.getElementById("km").value;
+        fascia_eta = document.getElementById("fascia_eta").value;
+    }
+)
