d1c45c59b27b04c848c776c2153e4377f0da1727
Luciano <lucco6823@gmail.com>
2021-07-22 17:59:55 +0200

    index completo

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 var btnGenera = document.getElementById("genera");
-var btnGenera = document.getElementById("annulla");
+var btnAnnulla = document.getElementById("annulla");
 console.log(btnGenera);
-var nome, km, fascia_eta;
+var nome, km, fascia_eta, sconto;
+var prezzo = km * 0.21;
+
+var Minorenne = document.getElementById("minorenne");
+var Maggiorenne = document.getElementById("maggiorenne");
+var Over65 = document.getElementById("over65");
 
 btnGenera.addEventListener("click",
     function(){
+        document.getElementById("biglietto").style.display="block";
+
         nome = document.getElementById("nome").value;
         km = document.getElementById("km").value;
         fascia_eta = document.getElementById("fascia_eta").value;
+
+        if(fascia_eta == "Minorenne"){
+            sconto = (prezzo / 100) * 20;
+            console.log(prezzo - sconto);
+        }
+        else if (fascia_eta == "Over65"){
+            sconto = (prezzo / 100) * 40;
+            console.log(prezzo - sconto);
+        }
+        else{
+            console.log(prezzo);
+        }
     }
 )
 
@@ -20,3 +39,4 @@ btnAnnulla.addEventListener("click",
         document.getElementById("km").value = " ";
         document.getElementById("fascia_eta").value = " ";
     }
+) 
