ad83dd180263696e3bba5f346e4800caed11352f
Luciano <lucco6823@gmail.com>
2021-07-22 16:34:41 +0200

    Parte grafica completa

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var btnGenera = document.getElementById("genera");
+var btnGenera = document.getElementById("annulla");
 console.log(btnGenera);
 var nome, km, fascia_eta;
 
 btnGenera.addEventListener("click",
-    function{
+    function(){
         nome = document.getElementById("nome").value;
         km = document.getElementById("km").value;
         fascia_eta = document.getElementById("fascia_eta").value;
     }
 )
+
+btnAnnulla.addEventListener("click",
+    function(){
+        biglietto.style.display = "none";
+
+        //svuoto input
+        document.getElementById("nome").value = " ";
+        document.getElementById("km").value = " ";
+        document.getElementById("fascia_eta").value = " ";
+    }
